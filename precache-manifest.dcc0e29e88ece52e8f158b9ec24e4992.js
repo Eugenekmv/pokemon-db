7e4b97236ae1cc71ae206e71000a08d64c292975
Eugene <lasttrip@ukr.net>
2020-08-27 17:08:51 +0300

    Updates

---
+++ precache-manifest.dcc0e29e88ece52e8f158b9ec24e4992.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa42812530dd358fdbcdf2dfc2f9781",
+    "url": "/pokemon-db/index.html"
+  },
+  {
+    "revision": "b8cdbc1952e49a814a67",
+    "url": "/pokemon-db/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a590124669501676f140",
+    "url": "/pokemon-db/static/js/2.781d87f3.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/pokemon-db/static/js/2.781d87f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8cdbc1952e49a814a67",
+    "url": "/pokemon-db/static/js/main.8afbfbb2.chunk.js"
+  },
+  {
+    "revision": "f0d4bcefe8ffb00423c9",
+    "url": "/pokemon-db/static/js/runtime-main.83d126aa.js"
+  },
+  {
+    "revision": "b3d23c0c6a547dc60457a0b2645fe38f",
+    "url": "/pokemon-db/static/media/bug.b3d23c0c.png"
+  },
+  {
+    "revision": "43fad52b2a95e44a04603dd22d3bed9a",
+    "url": "/pokemon-db/static/media/dark.43fad52b.png"
+  },
+  {
+    "revision": "3c95d0516b694173d6c4e81debf734f2",
+    "url": "/pokemon-db/static/media/dragon.3c95d051.png"
+  },
+  {
+    "revision": "a640e22236e689553b19d1e2d4ae4d2e",
+    "url": "/pokemon-db/static/media/electric.a640e222.png"
+  },
+  {
+    "revision": "d882ac5f21557cacb47c6c159f9bbafa",
+    "url": "/pokemon-db/static/media/fairy.d882ac5f.png"
+  },
+  {
+    "revision": "94e1c66095ad8f66ac0a8fec174bf13b",
+    "url": "/pokemon-db/static/media/fighting.94e1c660.png"
+  },
+  {
+    "revision": "eac89c9ab48cea86b79df8e1b82a264f",
+    "url": "/pokemon-db/static/media/fire.eac89c9a.png"
+  },
+  {
+    "revision": "31e9a60bf9b525fb01a52ba5327df4aa",
+    "url": "/pokemon-db/static/media/flying.31e9a60b.png"
+  },
+  {
+    "revision": "c3242dce17f9635ea8a1bdc4e941b6ab",
+    "url": "/pokemon-db/static/media/ghost.c3242dce.png"
+  },
+  {
+    "revision": "2b9b1bfc16d8b3170a9e04a5630c7ce0",
+    "url": "/pokemon-db/static/media/grass.2b9b1bfc.png"
+  },
+  {
+    "revision": "f3a61fdc6206dba2c388d62dffffb540",
+    "url": "/pokemon-db/static/media/ground.f3a61fdc.png"
+  },
+  {
+    "revision": "d03edb7e031f8e27874aa087b6aa34e2",
+    "url": "/pokemon-db/static/media/ice.d03edb7e.png"
+  },
+  {
+    "revision": "47d2e7b2ba5c5fb00fa075be9639cc21",
+    "url": "/pokemon-db/static/media/normal.47d2e7b2.png"
+  },
+  {
+    "revision": "f4486dcb1fabd0432bbf7753b0d20344",
+    "url": "/pokemon-db/static/media/psycic.f4486dcb.png"
+  },
+  {
+    "revision": "c6a84bf6791e5d43080473d604144e59",
+    "url": "/pokemon-db/static/media/rock.c6a84bf6.png"
+  },
+  {
+    "revision": "00b3109a5a68c54f67f9afacc4319445",
+    "url": "/pokemon-db/static/media/steel.00b3109a.png"
+  },
+  {
+    "revision": "e43fbc983ba9b7e061515d4024173a19",
+    "url": "/pokemon-db/static/media/water.e43fbc98.png"
+  }
+]);
